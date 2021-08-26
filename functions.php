6355f1594885c263d92caccecd37b0894e136ea6
webinnovationlabscom <89593590+webinnovationlabscom@users.noreply.github.com>
2021-08-26 17:52:15 +0300

    Create functions.php
    Custom functions added to the Hello Elementor theme for our client

---
+++ functions.php
@@ -0,0 +1,248 @@
+<?php
+/**
+ * Theme functions and definitions
+ *
+ * @package HelloElementor
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+define( 'HELLO_ELEMENTOR_VERSION', '2.3.1' );
+
+if ( ! isset( $content_width ) ) {
+	$content_width = 800; // Pixels.
+}
+
+if ( ! function_exists( 'hello_elementor_setup' ) ) {
+	/**
+	 * Set up theme support.
+	 *
+	 * @return void
+	 */
+	function hello_elementor_setup() {
+		$hook_result = apply_filters_deprecated( 'elementor_hello_theme_load_textdomain', [ true ], '2.0', 'hello_elementor_load_textdomain' );
+		if ( apply_filters( 'hello_elementor_load_textdomain', $hook_result ) ) {
+			load_theme_textdomain( 'hello-elementor', get_template_directory() . '/languages' );
+		}
+
+		$hook_result = apply_filters_deprecated( 'elementor_hello_theme_register_menus', [ true ], '2.0', 'hello_elementor_register_menus' );
+		if ( apply_filters( 'hello_elementor_register_menus', $hook_result ) ) {
+			register_nav_menus( array( 'menu-1' => __( 'Primary', 'hello-elementor' ) ) );
+		}
+
+		$hook_result = apply_filters_deprecated( 'elementor_hello_theme_add_theme_support', [ true ], '2.0', 'hello_elementor_add_theme_support' );
+		if ( apply_filters( 'hello_elementor_add_theme_support', $hook_result ) ) {
+			add_theme_support( 'post-thumbnails' );
+			add_theme_support( 'automatic-feed-links' );
+			add_theme_support( 'title-tag' );
+			add_theme_support(
+				'html5',
+				array(
+					'search-form',
+					'comment-form',
+					'comment-list',
+					'gallery',
+					'caption',
+				)
+			);
+			add_theme_support(
+				'custom-logo',
+				array(
+					'height'      => 100,
+					'width'       => 350,
+					'flex-height' => true,
+					'flex-width'  => true,
+				)
+			);
+
+			/*
+			 * Editor Style.
+			 */
+			add_editor_style( 'editor-style.css' );
+			
+			/*
+			 * Gutenberg wide images.
+			 */
+			add_theme_support( 'align-wide' );
+
+			/*
+			 * WooCommerce.
+			 */
+			$hook_result = apply_filters_deprecated( 'elementor_hello_theme_add_woocommerce_support', [ true ], '2.0', 'hello_elementor_add_woocommerce_support' );
+			if ( apply_filters( 'hello_elementor_add_woocommerce_support', $hook_result ) ) {
+				// WooCommerce in general.
+				add_theme_support( 'woocommerce' );
+				// Enabling WooCommerce product gallery features (are off by default since WC 3.0.0).
+				// zoom.
+				add_theme_support( 'wc-product-gallery-zoom' );
+				// lightbox.
+				add_theme_support( 'wc-product-gallery-lightbox' );
+				// swipe.
+				add_theme_support( 'wc-product-gallery-slider' );
+			}
+		}
+	}
+}
+add_action( 'after_setup_theme', 'hello_elementor_setup' );
+
+if ( ! function_exists( 'hello_elementor_scripts_styles' ) ) {
+	/**
+	 * Theme Scripts & Styles.
+	 *
+	 * @return void
+	 */
+	function hello_elementor_scripts_styles() {
+		$enqueue_basic_style = apply_filters_deprecated( 'elementor_hello_theme_enqueue_style', [ true ], '2.0', 'hello_elementor_enqueue_style' );
+		$min_suffix          = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+
+		if ( apply_filters( 'hello_elementor_enqueue_style', $enqueue_basic_style ) ) {
+			wp_enqueue_style(
+				'hello-elementor',
+				get_template_directory_uri() . '/style' . $min_suffix . '.css',
+				[],
+				HELLO_ELEMENTOR_VERSION
+			);
+		}
+
+		if ( apply_filters( 'hello_elementor_enqueue_theme_style', true ) ) {
+			wp_enqueue_style(
+				'hello-elementor-theme-style',
+				get_template_directory_uri() . '/theme' . $min_suffix . '.css',
+				[],
+				HELLO_ELEMENTOR_VERSION
+			);
+		}
+	}
+}
+add_action( 'wp_enqueue_scripts', 'hello_elementor_scripts_styles' );
+
+if ( ! function_exists( 'hello_elementor_register_elementor_locations' ) ) {
+	/**
+	 * Register Elementor Locations.
+	 *
+	 * @param ElementorPro\Modules\ThemeBuilder\Classes\Locations_Manager $elementor_theme_manager theme manager.
+	 *
+	 * @return void
+	 */
+	function hello_elementor_register_elementor_locations( $elementor_theme_manager ) {
+		$hook_result = apply_filters_deprecated( 'elementor_hello_theme_register_elementor_locations', [ true ], '2.0', 'hello_elementor_register_elementor_locations' );
+		if ( apply_filters( 'hello_elementor_register_elementor_locations', $hook_result ) ) {
+			$elementor_theme_manager->register_all_core_location();
+		}
+	}
+}
+add_action( 'elementor/theme/register_locations', 'hello_elementor_register_elementor_locations' );
+
+if ( ! function_exists( 'hello_elementor_content_width' ) ) {
+	/**
+	 * Set default content width.
+	 *
+	 * @return void
+	 */
+	function hello_elementor_content_width() {
+		$GLOBALS['content_width'] = apply_filters( 'hello_elementor_content_width', 800 );
+	}
+}
+add_action( 'after_setup_theme', 'hello_elementor_content_width', 0 );
+
+if ( is_admin() ) {
+	require get_template_directory() . '/includes/admin-functions.php';
+}
+
+if ( ! function_exists( 'hello_elementor_check_hide_title' ) ) {
+	/**
+	 * Check hide title.
+	 *
+	 * @param bool $val default value.
+	 *
+	 * @return bool
+	 */
+	function hello_elementor_check_hide_title( $val ) {
+		if ( defined( 'ELEMENTOR_VERSION' ) ) {
+			$current_doc = \Elementor\Plugin::instance()->documents->get( get_the_ID() );
+			if ( $current_doc && 'yes' === $current_doc->get_settings( 'hide_title' ) ) {
+				$val = false;
+			}
+		}
+		return $val;
+	}
+}
+add_filter( 'hello_elementor_page_title', 'hello_elementor_check_hide_title' );
+
+/**
+ * Wrapper function to deal with backwards compatibility.
+ */
+if ( ! function_exists( 'hello_elementor_body_open' ) ) {
+	function hello_elementor_body_open() {
+		if ( function_exists( 'wp_body_open' ) ) {
+			wp_body_open();
+		} else {
+			do_action( 'wp_body_open' );
+		}
+	}
+}
+
+// Shortcode to output custom PHP in Elementor In Yard
+function wpc_elementor_shortcode( $atts ) {
+   // Get the user's ID
+$user_id = get_current_user_id();
+
+// Insert the ID into the shortcode
+echo do_shortcode('[posts_table post_type="vehicle" columns="cf:vin, cf:year, cf:make, cf:model, cf:title_status, cf:lot_location, cf:lot_#, cf:weight, cf:status, cf:owner,button" shortcodes="true" cf="owner:' . $user_id . '+status:In Yard"]'); 
+}
+add_shortcode( 'my_elementor_php_output', 'wpc_elementor_shortcode');
+
+// Shortcode to output custom PHP in Elementor Active
+function wpc_elementor_shortcode7( $atts ) {
+   // Get the user's ID
+$user_id = get_current_user_id();
+
+// Insert the ID into the shortcode
+echo do_shortcode('[posts_table post_type="vehicle" columns="cf:vin, cf:year, cf:make, cf:model, cf:title_status, cf:lot_location, cf:lot_#, cf:weight, cf:status, cf:owner,button" shortcodes="true" cf="owner:' . $user_id . '+status:Active"]'); 
+}
+add_shortcode( 'my_elementor_php_output7', 'wpc_elementor_shortcode7');
+
+// Shortcode to output custom PHP in Elementor
+function wpc_elementor_shortcode1( $atts ) {
+   // Get the user's ID
+$user_id = get_current_user_id();
+
+// Insert the ID into the shortcode
+echo do_shortcode('[posts_table post_type="container" columns="cf:owner, cf:container_#, cf:reference_#, cf:equipment, cf:seal_#, cf:vehicle, cf:status, cf:gallery,button" shortcodes="true" cf="owner:' . $user_id . '"]'); 
+}
+add_shortcode( 'my_elementor_php_output1', 'wpc_elementor_shortcode1');
+
+add_action('check_admin_referer', 'logout_without_confirm', 10, 2);
+function logout_without_confirm($action, $result)
+{
+    /**
+     * Allow logout without confirmation
+     */
+    if ($action == "log-out" && !isset($_GET['_wpnonce'])) {
+        $redirect_to = isset($_REQUEST['redirect_to']) ? $_REQUEST['redirect_to'] : 'url-you-want-to-redirect';
+        $location = str_replace('&amp;', '&', wp_logout_url($redirect_to));
+        header("Location: $location");
+        die;
+    }
+}
+
+
+function my_post_title_updater( $post_id ) {
+
+	if ( get_post_type( $post_id ) == 'vehicle' ) {
+		
+		$my_post = array();
+		$my_post['ID'] = $post_id;
+		$my_post['post_title'] = get_field( 'vin', $post_id ) . ' - ' . substr(get_field( 'year', $post_id ), 0, 30) . '-'.substr(get_field( 'make', $post_id ), 0, 30) . '-'.substr(get_field( 'model', $post_id ), 0, 30);
+		
+		wp_update_post( $my_post );
+		
+
+	}
+
+}
+ 
+// run after ACF saves the $_POST['fields'] data
+add_action('acf/save_post', 'my_post_title_updater', 20);
